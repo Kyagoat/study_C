e70bd3c7c44269db299911c483755a686e432f8a
mehdiky <kylian.mehdi@etu.univ-grenoble.alpes.fr>
2024-09-26 08:18:35 +0200

    Arguments.c finished

--- LectureEcriture/arguments.c
+++ LectureEcriture/arguments.c
@@ -1,12 +1,34 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h> 
+#include <stdlib.h>
+
 int main(int argc, char* argv[])
 {
    char* inputFilename;
-   char* ouputFilename;
+   char* outputFilename;
    if (argc < 3){
       fprintf(stderr, "You should provide an input and an output filenames. Aborting");
       exit(-1);
    }
-
    inputFilename = argv[1];
    outputFilename = argv[2];
+
+   FILE* f = fopen(inputFilename, "r");
+   FILE* darker = fopen(outputFilename, "w");
+
+   int entier;
+
+
+   char line[200];
+   for(int i =0; i<=3; i++){
+      fgets(line, 200, f);
+      fprintf(darker, "%s", line);    
+   }
+      
+   
+   while(fscanf(f, "%d", &entier) == 1){
+      fprintf(darker, "%d\n", entier/6);
+   }
 }
