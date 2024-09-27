09da6000b7a7091c80fcc76774a9a1a43579951d
mehdiky <kylian.mehdi@etu.univ-grenoble.alpes.fr>
2024-09-27 11:59:41 +0200

    lighter.c finished

---
+++ LectureEcriture/lighter.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>  // For 'open'
+#include <unistd.h> // For 'write'
+#include <string.h> // For 'strlen'
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[]){
+
+    FILE* f = fopen("Chaton.ppm", "r");
+    FILE* darker = fopen("LightChaton.ppm", "w");
+
+    int entier;
+
+
+    char line[200];
+    for(int i =0; i<=3; i++){
+        fgets(line, 200, f);
+        fprintf(darker, "%s", line);
+    }
+
+
+    while(fscanf(f, "%d", &entier) == 1){
+        fprintf(darker, "%d\n", entier*4);
+    }
+
+}
